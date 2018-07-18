ad2daf1e84a9de5e496494c79b7d114eddab6b26
Jorge-Bau <jorge.bautista@tectijuana.edu.mx>
2018-07-18 12:35:35 -0700

    Pantalla Welcome y Tabla aeropuerto

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-import {Navbar, NavItem} from 'react-materialize'
+import { Navbar, NavItem, Button, Dropdown } from 'react-materialize'
 
 export default class Nav extends Component {
     render() {
         return (
             <div>
                 <Navbar brand='Journeys' left className='blue'>
-                    <NavItem onClick={() => console.log('test click')}>start</NavItem>
-                   
+                    <Dropdown trigger={
+                        <Button ><a class="btn-floating pulse blue"><i class="material-icons">menu</i></a></Button>
+                    }>
+                        <NavItem>Aeropuerto</NavItem>
+                        <NavItem>Vuelos</NavItem>
+                        <NavItem divider />
+                        <NavItem>Cerrar Sesion</NavItem>
+                    </Dropdown>
+
                 </Navbar>
             </div>
         )
