a182b944b0298ce0595c12a4bab286c24fe63d96
AdelatorreG <yunechaos0929@gmail.com>
2018-07-19 09:20:37 -0700

    Your Message

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,22 +4,16 @@ import {Footer} from 'react-materialize'
 export default () => {
   return (
     <div>
-      <Footer copyrights="&2018 ArkusNexus" 
-  moreLinks={
-    <a className="grey-text text-lighten-3 right" href="#!">More Links</a>
-  }
+      <Footer copyrights="Journeys ©2018 ArkusNexus" 
   links={
     <ul>
       <li><a className="grey-text text-lighten-3" href="#!">Avisos</a></li>
       <li><a className="grey-text text-lighten-3" href="#!">Privacidad</a></li>
-      <li><a className="grey-text text-lighten-3" href="#!">Contacto</a></li>
-      <li><a className="grey-text text-lighten-3" href="#!">Provedor</a></li>
+      <li><a className="grey-text text-lighten-3" href="#!">Terminos y Condiciones</a></li>
     </ul>
   }
   brand='Journeys' right className='blue'
 >
-    <h5 className="white-text">Journeys</h5>
-    <p className="grey-text text-lighten-4">Sueña, realiza, disfruta.</p>
 </Footer>;
     </div>
   )
