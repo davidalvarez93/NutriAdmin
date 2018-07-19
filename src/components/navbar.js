6b4f764dfefb3a158760ccf4506b1b7489f5a278
Jorge-Bau <jorge.bautista@tectijuana.edu.mx>
2018-07-19 10:15:49 -0700

    NavBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ export default class Nav extends Component {
             <div>
                 <Navbar brand='Journeys' left className='blue'>
                     <Dropdown trigger={
-                        <Button ><a class="btn-floating pulse blue"><i class="material-icons">menu</i></a></Button>
+                        <Button floating waves='light' className='blue'><i class="material-icons">dehaze</i></Button>
                     }>
                         <NavItem>Aeropuerto</NavItem>
                         <NavItem>Vuelos</NavItem>
