a074dae899f036ff1e846ebd1fad4a9db22a05dd
AdelatorreG <yunechaos0929@gmail.com>
2018-07-19 22:42:02 -0700

    C:/Program Files/Git/Final Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import {Footer} from 'react-materialize'
 export default () => {
   return (
     <div>
-      <Footer copyrights="Journeys ©2018 ArkusNexus" 
+      <Footer copyrights="©2018 Journeys.com"
   links={
     <ul>
       <li><a className="grey-text text-lighten-3" href="#!">Avisos</a></li>
@@ -14,6 +14,8 @@ export default () => {
   }
   brand='Journeys' right className='blue'
 >
+    <h5 className="white-text">Journeys</h5>
+    <p className="grey-text text-lighten-4">Donde quieras, Cuando quieras, Como quieras.</p>
 </Footer>;
     </div>
   )
