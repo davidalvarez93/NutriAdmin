e1714c3c49e3c2f4bbf8f6b6c3d0f4467326169e
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2018-07-20 12:03:21 -0700

    Render de welcome en index.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -19,4 +19,3 @@ const Navigation = () =>
 
 
 export default Navigation;
-export default ToWelcome;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Nav from './components/navbar';
 import registerServiceWorker from './registerServiceWorker';
+
+import Welcome from './components/welcomeforms';
+import Nav from './components/navbar';
 import Login from './components/loginforms';
 import Footer from './components/footer';
 
+ReactDOM.render(<Welcome/>, document.getElementById('wel'));
 ReactDOM.render(<Nav />, document.getElementById('nav'));
 ReactDOM.render(<Login />, document.getElementById('lg'));
 ReactDOM.render(<Footer/>, document.getElementById('ftr'));
 registerServiceWorker();
 
-
+/*
 
 const express = require('express');
 const morgan = require('morgan');
@@ -40,4 +43,4 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')}`);
-});
+});*/
