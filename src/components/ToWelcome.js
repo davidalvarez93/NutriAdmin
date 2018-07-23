3f86b44ada048b5aa5cbe74af75b3b5ff4391deb
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2018-07-22 18:39:00 -0700

    LogIn

--- src/components/ToWelcome.js
+++ src/components/ToWelcome.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 import * as routes from '../constants/routes';
 
-const Navigation = () =>
+
+  const ToWelcome = () =>
   <div>
     <ul>
-      <li><Link to={routes.LOG_IN}>Sign In</Link></li>
+      <li><Link to={routes.WELCOME}>Log In</Link></li>
     </ul>
   </div>
 
 
-
-export default Navigation;
+export default ToWelcome;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import Nav from './components/navbar';
 import Login from './components/loginforms';
 import Footer from './components/footer';
 
-ReactDOM.render(<Welcome/>, document.getElementById('wel'));
+//ReactDOM.render(<Welcome/>, document.getElementById('wel'));
+
 ReactDOM.render(<Nav />, document.getElementById('nav'));
 ReactDOM.render(<Login />, document.getElementById('lg'));
 ReactDOM.render(<Footer/>, document.getElementById('ftr'));
