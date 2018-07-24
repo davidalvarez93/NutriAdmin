cd16f9671af87cf786231489d18fdfa7fd7bb6e0
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2018-07-23 18:19:31 -0700

    + Correccion del MERGE

--- src/components/index.js
+++ src/components/index.js
@@ -7,13 +7,12 @@ import Welcome from './components/welcomeforms';
 import Nav from './components/navbar';
 import Login from './components/loginforms';
 import Footer from './components/footer';
-import App from './components/App';
 
 //ReactDOM.render(<Welcome/>, document.getElementById('wel'));
-ReactDOM.render(<App />, document.getElementById('app'));
+
 ReactDOM.render(<Nav />, document.getElementById('nav'));
-//ReactDOM.render(<Login />, document.getElementById('lg'));
-//ReactDOM.render(<Footer/>, document.getElementById('ftr'));
+ReactDOM.render(<Login />, document.getElementById('lg'));
+ReactDOM.render(<Footer/>, document.getElementById('ftr'));
 registerServiceWorker();
 
 /*
