f44033fa1e7eeb31e0638e43fdfb7aebbbaa848f
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2018-08-01 21:37:37 -0700

    +Paginacion

--- src/components/Airports.js
+++ src/components/Airports.js
@@ -118,6 +118,15 @@ class Airport extends Component{
                                 }
                             </tbody>
                         </table>
+                        <ul class="pagination center">
+                            <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                            <li class="active"><a href="#!">1</a></li>
+                            <li class="waves-effect"><a href="#!">2</a></li>
+                            <li class="waves-effect"><a href="#!">3</a></li>
+                            <li class="waves-effect"><a href="#!">4</a></li>
+                            <li class="waves-effect"><a href="#!">5</a></li>
+                            <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
