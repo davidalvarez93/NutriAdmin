1facaaa62fca6003ad73c56e62b79086a8fd3837
scorbac <jose.lopez17@tectijuana.edu.mx>
2018-08-02 12:38:24 -0700

    limpieza

--- src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const REGISTER = '/register';
-export const LOG_IN = './loginforms';
-export const LANDING = '/';
-export const WELCOME = './welcomeforms';
-export const ACCOUNT = '/account';
-export const PASSWORD_FORGET = '/pw-forget';
